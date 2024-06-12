aaedeb12082f819ed3bf3d9ca5c20ffee6226624
Nithya <Nithya.sivaprakasam@syncfusion.com>
2024-06-12 16:13:27 +0530

    Adding sample for webmethodadaptor and urladaptor

---
+++ UrlAdaptor/UrlAdaptor/wwwroot/js/index.js
@@ -0,0 +1,26 @@
+﻿ej.grids.Grid.Inject(ej.grids.Toolbar, ej.grids.Edit, ej.grids.Filter, ej.grids.Page, ej.grids.Sort);
+
+var data = new ej.data.DataManager({
+    url: 'https://localhost:7073/api/Grid',
+    insertUrl: 'https://localhost:7073/api/Grid/Insert',
+    updateUrl: 'https://localhost:7073/api/Grid/Update',
+    removeUrl: 'https://localhost:7073/api/Grid/Remove',
+    adaptor: new ej.data.UrlAdaptor()
+});
+
+var grid = new ej.grids.Grid({
+    dataSource: data,
+    allowPaging: true,
+    allowSorting: true,
+    allowFiltering: true,
+    toolbar: ['Add', 'Edit', 'Delete', 'Update', 'Cancel', 'Search'],
+    editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal' },
+    columns: [
+        { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120, isPrimaryKey: true, type: 'number' },
+        { field: 'CustomerID', width: 140, headerText: 'Customer ID', type: 'string' },
+        { field: 'ShipCity', headerText: 'ShipCity', width: 140 },
+        { field: 'ShipCountry', headerText: 'ShipCountry', width: 140 }
+    ]
+});
+
+grid.appendTo('#Grid');
