e2f29ab64c64e7941030dce593be0d344ee37cbf
JamunaSundaramSF3699 <jamuna.sundaram@syncfusion.com>
2025-04-24 16:33:05 +0530

    documentation(914729):Updated custom adaptor sample

---
+++ CustomAdaptor/CustomAdaptor/wwwroot/js/CustomAdaptor.js
@@ -0,0 +1,22 @@
+﻿export class CustomAdaptor extends ej.data.ODataV4Adaptor {
+    processQuery(dm, query) {
+        dm.dataSource.url = 'https://localhost:7295/odata/orders'; //Change the url.
+        query.addParams('Syncfusion Grid', 'true'); // Add the additional parameter.
+        return super.processQuery.apply(this, arguments);
+    }
+
+    beforeSend(dm, request, settings) {
+        request.headers.set('Authorization', `true`);
+        super.beforeSend(dm, request, settings);
+    }
+
+    processResponse() {
+        let i = 0;
+        const original = super.processResponse.apply(this, arguments);
+        // Adding serial number.
+        if (original.result) {
+            original.result.forEach((item) => ej.base.setValue('SNo', ++i, item));
+        }
+        return original;
+    }
+}
---
+++ CustomAdaptor/CustomAdaptor/wwwroot/js/index.js
@@ -0,0 +1,26 @@
+﻿
+import { CustomAdaptor } from './CustomAdaptor.js';
+
+ej.grids.Grid.Inject(ej.grids.Toolbar, ej.grids.Filter, ej.grids.Sort, ej.grids.Page, ej.grids.Edit);
+
+var data = new ej.data.DataManager({
+    url: 'https://localhost:7295/odata/orders', // Here xxxx represents the port number
+    adaptor: new CustomAdaptor()
+});
+
+var grid = new ej.grids.Grid({
+    dataSource: data,
+    allowPaging:true,
+    allowFiltering: true,
+    allowSorting: true,
+    toolbar: ['Add', 'Edit', 'Update', 'Delete', 'Cancel', 'Search'],
+    editSettings: { allowAdding: true, allowDeleting: true, allowEditing: true },
+    columns: [
+        { field: 'OrderID', headerText: 'Order ID', textAlign: 'Right', width: 120, isPrimaryKey: true, type: 'number' },
+        { field: 'CustomerID', width: 140, headerText: 'Customer ID', type: 'string' },
+        { field: 'EmployeeID', headerText: 'Employee ID', width: 140 },
+        { field: 'ShipCountry', headerText: 'ShipCountry', width: 140 }
+    ]
+});
+
+grid.appendTo('#Grid');
